b8d6c5d6018e48e54fa52027220a9cad9deb2a9e
B11256022 <qqa0925931225@gmail.com>
2024-03-15 11:11:04 +0800

    我新增了一個檔案

---
+++ B11256022-2/2.js
@@ -0,0 +1,7 @@
+var a = 10;
+let b = 20;
+var myName = "林小妹";
+var age = 18;
+document.write('<h1> 我是\'林小妹\'，今年'+age+'歲')
+document.write('<h1> 數字:', a, '+', b, ' = ',a+b)
+document.write('<h1> 字串:', a, '+', b, ' = '+a+b)
